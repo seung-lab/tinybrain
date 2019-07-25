3f74f4e58bae6d0b2cf675d5de91f841d33515e1
William Silversmith <william.silversmith@gmail.com>
2019-07-25 00:54:37 -0400

    install: add stdlib and min os version for compiling on mac

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 import setuptools
+import sys 
 
 # NOTE: If accelerated.cpp does not exist:
 # cython -3 --fast-fail -v --cplus tinybrain/accelerated.pyx
 
 import numpy as np
+
+
+extra_compile_args = [
+  '-std=c++11', '-O3', '-msse3',
+  # '-DCYTHON_TRACE=1'
+]
+
+if sys.platform == 'darwin':
+  extra_compile_args += [ '-stdlib=libc++', '-mmacosx-version-min=10.9' ]
 
 setuptools.setup(
   setup_requires=['pbr', 'numpy'],
@@ -18,10 +28,7 @@ setuptools.setup(
       sources=[ 'tinybrain/accelerated.cpp' ],
       language='c++',
       include_dirs=[ np.get_include() ],
-      extra_compile_args=[
-        '-std=c++11', '-O3', '-ffast-math', '-msse3',
-        # '-DCYTHON_TRACE=1'
-      ]
+      extra_compile_args=extra_compile_args,
     )
   ],
   long_description_content_type='text/markdown',
