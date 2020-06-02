4183359660e70e3c5592285eac22d56532141e50
William Silversmith <william.silversmith@gmail.com>
2020-06-02 09:37:50 -0400

    perf: accelerated 2x2x2 segmentation downsample (#8)
    * perf: accelerated 2x2x2 segmentation downsample
    
    * perf: use hybrid counting / countless strategy

--- tinybrain/accelerated.hpp
+++ tinybrain/accelerated.hpp
@@ -411,6 +411,77 @@ inline void shift_right(T* accum, const size_t ovoxels, const size_t bits) {
   }
 }
 
+// MODE POOLING 2x2x2
+// based on code by Chris Jordan, 2017
+
+template <typename T>
+inline void _mode_pooling_2x2x2(
+  T* img, T* oimg,
+  const size_t sx, const size_t sy, 
+  const size_t sz, const size_t sw = 1
+) {
+
+  const size_t sxy = sx * sy;
+
+  const size_t osx = (sx + 1) >> 1;
+  const size_t osy = (sy + 1) >> 1;
+  const size_t osxy = osx * osy;
+
+  T vals[8];
+  T cur_val, max_val;
+  size_t max_ct, cur_ct;
+
+  for (size_t z = 0; z < sz; z += 2) {
+    for (size_t y = 0; y < sy; y += 2) {
+      for (size_t x = 0; x < sx; x += 2) {
+        size_t offset = x + sx * y + sxy * z;
+        
+        size_t plus_x = (x < sx - 1);
+        size_t plus_y = (y < sy - 1) * sx;
+        size_t plus_z = (z < sz - 1) * sxy;
+
+        vals[0] = img[ offset ];
+        vals[1] = img[ offset + plus_x ];
+        vals[2] = img[ offset + plus_y ];
+        vals[3] = img[ offset + plus_x + plus_y ];
+        vals[4] = img[ offset + plus_z ];
+        vals[5] = img[ offset + plus_x + plus_z ];
+        vals[6] = img[ offset + plus_y + plus_z ];
+        vals[7] = img[ offset + plus_x + plus_y + plus_z ];
+        
+        size_t o_loc = (x >> 1) + osx * (y >> 1) + osxy * (z >> 1);
+        // These two if statements could be removed, but they add a very small
+        // cost on random data (< 10%) and can speed up connectomics data by ~4x
+        if (vals[0] == vals[1] && vals[0] == vals[2] && vals[0] == vals[3]) {
+          oimg[o_loc] = vals[0];
+          continue;
+        }
+        else if (vals[4] == vals[5] && vals[4] == vals[6] && vals[4] == vals[7]) {
+          oimg[o_loc] = vals[4];
+          continue;
+        }
+
+        max_ct = 0;
+        for (short int t = 0; t < 8; t++) {
+          cur_val = vals[t];
+          cur_ct = 0;
+          for (short int p = 0; p < 8; p++) {
+            cur_ct += (cur_val == vals[p]);
+          }
+
+          if (cur_ct > max_ct) {
+              max_ct = cur_ct;
+              max_val = cur_val;
+          }
+        }
+
+        oimg[o_loc] = max_val;
+      }
+    }
+  }
+}
+
+
 };
 
 #endif
