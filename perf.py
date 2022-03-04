49fa960e19ed7ebaa0a57fbebaff7564b6a945aa
William Silversmith <william.silversmith@gmail.com>
2022-03-03 23:48:45 -0500

    docs: add results of a perf run

--- perf.py
+++ perf.py
@@ -1,3 +1,21 @@
+# Results on a 2021 Macbook Pro M1
+# AVG POOLING ((2048, 2048, 64), uint8)
+# 2x2 1 mip: 0.693s, 3868.58 MVx/sec, N=10
+# 2x2 2 mip: 0.996s, 2691.03 MVx/sec, N=10
+# 2x2x2 1 mip: 0.600s, 4466.76 MVx/sec, N=10
+# 2x2x2 2 mip: 0.939s, 2855.51 MVx/sec, N=10
+# 2x2x2 1 mip sparse: 1.914s, 1400.17 MVx/sec, N=10
+# 2x2x2 2 mip sparse: 2.110s, 1270.18 MVx/sec, N=10
+# MODE POOLING ((2048, 2048, 64), uint64)
+# 2x2 1 mip: 9.830s, 272.63 MVx/sec, N=10
+# 2x2 2 mip: 16.274s, 164.68 MVx/sec, N=10
+# 2x2 1 mip sparse: 19.389s, 138.22 MVx/sec, N=10
+# 2x2 2 mip sparse: 32.603s, 82.20 MVx/sec, N=10
+# 2x2x2 1 mip: 7.935s, 337.74 MVx/sec, N=10
+# 2x2x2 2 mip: 8.951s, 299.42 MVx/sec, N=10
+# 2x2x2 1 mip sparse: 603.875s, 4.44 MVx/sec, N=10
+# 2x2x2 2 mip sparse: 663.919s, 4.04 MVx/sec, N=10
+
 import numpy as np
 import tinybrain
 import time
