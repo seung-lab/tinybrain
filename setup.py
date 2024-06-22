7978ea77cb3e2dcf2424bd3106024661a816825a
William Silversmith <william.silversmith@gmail.com>
2024-06-22 17:22:53 -0400

    ci: rebuild for numpy 2.0

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ if sys.platform == 'darwin':
 setuptools.setup(
   setup_requires=['pbr', 'numpy','cython'],
   install_requires=['numpy'],
-  python_requires=">=3.7",
+  python_requires=">=3.8",
   ext_modules=[
     setuptools.Extension(
       'tinybrain.accelerated',
