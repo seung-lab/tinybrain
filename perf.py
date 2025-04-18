e3a7ce301f9bbb713852e585f140aa37ffb92753
William Silversmith <william.silversmith@gmail.com>
2025-04-18 19:37:17 -0400

    test: update benchmark with Macbook Pro M3 + add pillow, opencv

--- perf.py
+++ perf.py
@@ -27,6 +27,26 @@
 # 2x2x2 1 mip sparse: 0.656s, 2041.96 MVx/sec, N=10
 # 2x2x2 2 mip sparse: 0.762s, 1758.42 MVx/sec, N=10
 
+# Results on a 2023 Macbook Pro M3
+# AVG POOLING ((2048, 2048, 64), uint8)
+# 2x2 1 mip: 0.084s, 31954.51 MVx/sec, N=10
+# 2x2 2 mip: 0.223s, 11995.13 MVx/sec, N=10
+# 2x2x2 1 mip: 0.042s, 63958.66 MVx/sec, N=10
+# 2x2x2 2 mip: 0.218s, 12303.19 MVx/sec, N=10
+# 2x2x2 1 mip sparse: 0.581s, 4612.79 MVx/sec, N=10
+# 2x2x2 2 mip sparse: 0.686s, 3908.00 MVx/sec, N=10
+# 2x2 1 mip pillow: 10.526s, 254.61 MVx/sec, N=10
+# 2x2 1 mip opencv: 5.352s, 500.79 MVx/sec, N=10
+# MODE POOLING ((2048, 2048, 64), uint64)
+# 2x2 1 mip: 1.030s, 2602.65 MVx/sec, N=10
+# 2x2 2 mip: 1.181s, 2269.54 MVx/sec, N=10
+# 2x2 1 mip sparse: 14.294s, 187.49 MVx/sec, N=10
+# 2x2 2 mip sparse: 25.615s, 104.63 MVx/sec, N=10
+# 2x2x2 1 mip: 3.621s, 740.07 MVx/sec, N=10
+# 2x2x2 2 mip: 4.066s, 659.13 MVx/sec, N=10
+# 2x2x2 1 mip sparse: 4.356s, 615.27 MVx/sec, N=10
+# 2x2x2 2 mip sparse: 5.084s, 527.10 MVx/sec, N=10
+
 import numpy as np
 import tinybrain
 import time
@@ -79,6 +99,33 @@ def benchmark_avg_pooling():
 		tinybrain.downsample_with_averaging(data, (2,2,2), num_mips=2, sparse=True)
 	end = time.time()
 	result("2x2x2 2 mip sparse", end - start, data, N)
+
+	try:
+		from PIL import Image
+		s = time.time()
+		out = np.zeros([1024,1024,64], dtype=np.uint8, order="F")
+		for _ in range(N):
+			for z in range(data.shape[2]):
+			  pilimg = Image.fromarray(data[:,:,z])
+			  out[:,:,z] = pilimg.resize( (1024, 1024) )
+		end = time.time()
+		result("2x2 1 mip pillow", end - start, data, N)
+		del out
+	except ImportError:
+		print("Pillow not installed.")
+
+	try:
+		import cv2
+		start = time.time()
+		out = np.zeros(shape=(1024,1024,64))
+		for _ in range(N):
+			for z in range(data.shape[2]):
+			  out[:,:,z] = cv2.resize(data[:,:,z], dsize=(1024, 1024) )
+		end = time.time()
+		result("2x2 1 mip opencv", end - start, data, N)
+		del out
+	except ImportError:
+		print("opencv not installed.")
 
 def benchmark_mode_pooling():
 	data = np.random.randint(1000,1255, size=(2048,2048,64), dtype=np.uint64)
